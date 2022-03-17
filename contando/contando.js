8bbd2c7087af706e8b95b538eeedd33979504f3f
Fabio S. Silva <fabio_silva76@outlook.com>
2022-03-17 13:00:37 -0300

    Update contando.js

--- contando/contando.js
+++ contando/contando.js
@@ -1,3 +1,12 @@
 function contar(){
-    
+    let saida = document.getElementById('saida')
+    saida.innerHTML += `<h2>Contando de 1 a 10</h2>`
+
+    let cont = 1
+
+    while( cont <= 10){
+        saida.innerHTML += `${cont} &#x1F449;`
+        cont ++ // corresponde a cont = cont + 1 
+    }
+    saida.innerHTML += `&#x1F31`
 }
